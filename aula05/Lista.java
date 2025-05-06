38bd86d8367fee63e963b195dde13e171a0fea8e
Valério Santos <130845898+valeriouscv@users.noreply.github.com>
2025-05-06 10:14:09 -0100

    Lista Simplesmente Ligada

---
+++ aula05/Lista.java
@@ -0,0 +1,55 @@
+package aula05;
+
+public class Lista {
+    //atributos
+    private No inicio;
+    private No fim;
+
+    //construtor
+    public Lista() {
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    //inserirIni
+    public void inserirIni(int info){
+        //escrever codigo
+    }
+    //inserirFim
+    public void inserirFim(int info){
+        //escrever codigo
+    }
+
+    //inserir em qualquer posicao
+    public void inserirPos(int pos, int info){
+        //escrever codigo
+    }
+
+    //listar
+    public void listar(){
+        //escrever codigo
+    }
+
+    //remover - inicio da lista
+    public void removerIni(){
+        //escrever codigo
+    }
+    //remover - fim da lista
+    public void removerFim(){
+        //escrever codigo
+    }
+    //remover - em qualquer posicao da lista
+    public void removerPos(int pos){
+        //escrever codigo
+    }
+
+    public void pesquisar(int info){
+        //escrever codigo
+    }
+
+    //vazia
+    private boolean vazia(){
+        return (this.inicio==null || this.fim == null);
+    }
+    
+}
---
+++ aula05/ListaTeste.java
@@ -0,0 +1,18 @@
+package aula05;
+
+public class ListaTeste {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        /*lista.inserirIni(9111111);
+        lista.inserirFim(9999999);
+        lista.inserirPos(3,9333333);
+        lista.pesquisar(9222222);
+        lista.listar();
+        lista.removerIni();
+        lista.listar();
+        lista.removerFim();
+        lista.listar();
+        lista.removerPos(2);
+        lista.listar();*/
+    }
+}
